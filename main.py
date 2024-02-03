f4b8c60d557f9b6893fc96a4b001eaba0503a87c
fujiN10 <151755066+fujiN10@users.noreply.github.com>
2024-02-03 18:54:43 +0400

    Большая задача по Maps API. Часть №1

---
+++ main.py
@@ -0,0 +1,12 @@
+from geocoder import get_ll_span
+from mapapi_PG import show_map
+
+toponym_to_find = 'Тольятти'
+
+if toponym_to_find:
+    ll, spn = get_ll_span(toponym_to_find)
+    add_params = f'pt={ll}'
+    ll_spn = f'll={ll}&spn={spn}'
+    show_map(ll_spn=ll_spn, map_type="map", add_params=add_params)
+else:
+    print('No toponym')
